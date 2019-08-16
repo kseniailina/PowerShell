e25a7d09f22cda2fe404cc97345b97b079d30673
kseniailina <50660900+kseniailina@users.noreply.github.com>
2019-08-16 18:28:31 +0300

    Add files via upload

---
+++ ParameterTest.ps1
@@ -0,0 +1,17 @@
+param(
+    [parameter(Mandatory=$true)] [string]$arg1,
+    [parameter(Mandatory=$true)] [string]$arg2,
+    [parameter(Mandatory=$false)] [int]$arg3=2,
+    [parameter(Mandatory=$false)] [int]$arg4=3
+)
+
+Function ParameterTest
+{
+    $total = $arg3+$arg4
+    
+    Write-Host $arg1
+    Write-Host $arg2
+    Write-Host $total
+}
+
+ParameterTest
